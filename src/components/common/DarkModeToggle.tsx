2f698b2115152f61cd6af692aa0393b08f93589d
Renie Adjani <renieadjani4@gmail.com>
2025-09-04 04:36:17 +0800

    new4

--- src/components/common/DarkModeToggle.tsx
+++ src/components/common/DarkModeToggle.tsx
@@ -11,17 +11,7 @@ const DarkModeToggle = () => {
   useEffect(() => setMounted(true), []);
 
   if (!mounted)
-    return (
-      <Image
-        src="data:image/svg+xml;base64,PHN2ZyBzdHJva2U9IiNGRkZGRkYiIGZpbGw9IiNGRkZGRkYiIHN0cm9rZS13aWR0aD0iMCIgdmlld0JveD0iMCAwIDI0IDI0IiBoZWlnaHQ9IjIwMHB4IiB3aWR0aD0iMjAwcHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+PHJlY3Qgd2lkdGg9IjIwIiBoZWlnaHQ9IjIwIiB4PSIyIiB5PSIyIiBmaWxsPSJub25lIiBzdHJva2Utd2lkdGg9IjIiIHJ4PSIyIj48L3JlY3Q+PC9zdmc+Cg=="
-        width={16}
-        height={16}
-        sizes="36x36"
-        alt="Loading Light/Dark Toggle"
-        priority={false}
-        title="Loading Light/Dark Toggle"
-      />
-    );
+    return <div className="border rounded-full p-[7px] border-transparent" />;
 
   return (
     <button
