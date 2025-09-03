c6dae5b9fb69894241910b8e2b6cf643d3d73d4f
Renie Adjani <renieadjani4@gmail.com>
2025-09-04 03:04:27 +0800

    new1

---
+++ src/lib/auth.ts
@@ -0,0 +1,96 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import GitHub from "next-auth/providers/github";
+import connectToDB from "./connectToDB";
+import { User } from "./models/user.model";
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  // session: { strategy: "jwt" },
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async signIn({ account, profile }) {
+      // console.log(account, profile);
+      // return true;
+      if (account?.provider === "google") {
+        await connectToDB();
+        try {
+          const user = await User.findOne({ email: profile?.email });
+          if (!user) {
+            await User.create({
+              name: profile?.name,
+              email: profile?.email,
+              image: profile?.picture,
+            });
+          }
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      }
+
+      if (account?.provider === "github") {
+        await connectToDB();
+        try {
+          const user = await User.findOne({ email: profile?.email });
+          if (!user) {
+            await User.create({
+              name: profile?.login,
+              email: profile?.email,
+              image: profile?.avatar_url,
+            });
+          }
+        } catch (error) {
+          console.log(error);
+          return false;
+        }
+      }
+
+      return true;
+    },
+    async jwt({ token }) {
+      //   console.log("token", token);
+      //   return token;
+      await connectToDB();
+      const existingUser = await User.findOne({ email: token.email });
+
+      if (existingUser) {
+        token.id = existingUser._id.toString();
+        token.name = existingUser.name;
+        token.email = existingUser.email;
+        token.image = existingUser.image;
+        token.isAdmin = existingUser.isAdmin;
+        if (existingUser.username) {
+          token.username = existingUser.username;
+        } else {
+          delete token.username;
+        }
+      }
+      // console.log("token", token);
+      return token;
+    },
+    async session({ session, token }) {
+      // console.log("session", session);
+      // console.log("token", token);
+      // return session;
+      session.user.id = token.id ?? "";
+      session.user.name = token.name ?? "";
+      session.user.email = token.email ?? "";
+      session.user.image = token.image ?? "";
+      session.user.isAdmin = token.isAdmin ?? false;
+      if (token.username) {
+        session.user.username = token.username ?? "";
+      }
+      // console.log("session", session);
+      return session;
+    },
+  },
+});
